28c2cea07de482174cf4ce52a402422df696f367
rakin406 <rakinhacker@gmail.com>
2023-12-28 00:03:26 +0600

    feat: Fix shit

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <qt6/QtGui/QGuiApplication>
 
-int main() {
-    std::cout << "Hello World\n";
-    return 0;
+int main(int argc, char* argv[]) {
+    QGuiApplication app(argc, argv);
+
+    return app.exec();
 }

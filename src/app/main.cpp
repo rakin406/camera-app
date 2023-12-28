9ebfe137cc0688df4d1ffec7ee5978afdf3aebe6
rakin406 <rakinhacker@gmail.com>
2023-12-29 00:38:28 +0600

    feat: Add headers

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,12 +1,11 @@
-#include <qt6/QtCore/QUrl>
-#include <qt6/QtQuickWidgets/QQuickWidget>
+/**
+ * @file
+ * @brief Entrypoint for the application.
+ */
+
 #include <qt6/QtWidgets/QApplication>
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
-    QQuickWidget* view = new QQuickWidget;
-    view->setSource(QUrl::fromLocalFile("myqmlfile.qml"));
-    view->show();
-
     return app.exec();
 }

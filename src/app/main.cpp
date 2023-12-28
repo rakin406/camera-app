b22b3d42a11019be8384eb5bd0cfbcc7847e0a9e
rakin406 <rakinhacker@gmail.com>
2023-12-28 13:51:17 +0600

    feat: Create application window

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,7 +1,12 @@
-#include <qt6/QtGui/QGuiApplication>
+#include <qt6/QtCore/QUrl>
+#include <qt6/QtQuickWidgets/QQuickWidget>
+#include <qt6/QtWidgets/QApplication>
 
 int main(int argc, char* argv[]) {
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
+    QQuickWidget* view = new QQuickWidget;
+    view->setSource(QUrl::fromLocalFile("myqmlfile.qml"));
+    view->show();
 
     return app.exec();
 }

5b46706fcc82908d9b465a71c764531e7665a4ea
rakin406 <rakinhacker@gmail.com>
2023-12-29 23:04:17 +0600

    feat: Add logging

--- src/api/models/camera.cpp
+++ src/api/models/camera.cpp
@@ -6,17 +6,27 @@
 #include "api/models/camera.h"
 #include "ui_camera.h"
 
+#include <qlogging.h>
 #include <QCamera>
 #include <QCameraDevice>
 
 namespace api {
 
-Camera::Camera() :
-    m_ui { new Ui::Camera },
-    // TODO: Allow front and back cameras.
-    m_camera { new QCamera { QCameraDevice::FrontFace } } {
+// TODO: Allow front and back cameras.
+Camera::Camera()
+    : m_ui { new Ui::Camera }
+    , m_camera { new QCamera { QCameraDevice::FrontFace } } {
+    qInfo("Camera model loaded.");
+
     m_ui->setupUi(this);
+    if (m_ui) {
+        qInfo("Camera UI loaded.");
+    } else {
+        qCritical("Camera UI failed to load.");
+    }
+
     m_camera->start();
+    qInfo("Camera started.");
 }
 
 }  // namespace api

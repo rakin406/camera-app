ffa3c4cc1f646d154b565bec21ce6408454fde39
rakin406 <rakinhacker@gmail.com>
2023-12-29 16:34:01 +0600

    feat: Add camera class impl

--- include/api/models/camera.h
+++ include/api/models/camera.h
@@ -7,9 +7,16 @@
 #define CAMERA_H
 
 #include <QAbstractItemModel>
+#include <QCamera>
+#include <QImageCapture>
+#include <QMediaRecorder>
+#include <QScopedPointer>
+
+namespace api::models {
 
 class Camera : public QAbstractItemModel {
     Q_OBJECT
+
 public:
     explicit Camera(QObject* parent = nullptr);
 
@@ -17,6 +24,25 @@ public:
     int columnCount(const QModelIndex& parent = QModelIndex()) const override;
     QVariant data(const QModelIndex& index,
                   int role = Qt::DisplayRole) const override;
+
+private slots:
+    // void record();
+    // void pause();
+    // void stop();
+    // void takeImage();
+
+private:
+    api::models::Camera* m_model;
+
+    QScopedPointer<QCamera> m_camera {};
+    QScopedPointer<QImageCapture> m_imageCapture {};
+    QScopedPointer<QMediaRecorder> m_mediaRecorder {};
+
+    QString m_videoContainerFormat {};
+    bool m_isCapturingImage { false };
+    bool m_isAppExiting { false };
 };
+
+}  // namespace api::models
 
 #endif
--- src/api/models/camera.cpp
+++ src/api/models/camera.cpp
@@ -4,3 +4,24 @@
  */
 
 #include "api/models/camera.h"
+
+#include <QAbstractItemModel>
+
+namespace api::models {
+
+Camera::Camera(QObject* parent) : QAbstractItemModel { parent } {
+}
+
+int Camera::rowCount(const QModelIndex& /*parent*/) const {
+    return 2;
+}
+
+int Camera::columnCount(const QModelIndex& /*parent*/) const {
+    return 3;
+}
+
+QVariant Camera::data(const QModelIndex& index, int role) const {
+    return QVariant();
+}
+
+}  // namespace api::models
